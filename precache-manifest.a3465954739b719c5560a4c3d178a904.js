94c9fb7f67b611da0b1cf15b63d91d0fc8c7a927
ganesh <ganeshdalei1992@gmail.com>
2019-10-13 04:27:45 +0530

    Updates

---
+++ precache-manifest.a3465954739b719c5560a4c3d178a904.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9eed8c0db6479186bccfd98218bf555",
+    "url": "/DashBoard/index.html"
+  },
+  {
+    "revision": "43af3b0d93a637fba0e2",
+    "url": "/DashBoard/static/css/main.26aca678.chunk.css"
+  },
+  {
+    "revision": "baaefd20239b132b0d94",
+    "url": "/DashBoard/static/js/2.411043fb.chunk.js"
+  },
+  {
+    "revision": "43af3b0d93a637fba0e2",
+    "url": "/DashBoard/static/js/main.f8fd5a42.chunk.js"
+  },
+  {
+    "revision": "3d32ffab5eca0665c48d",
+    "url": "/DashBoard/static/js/runtime-main.1f0bfd47.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DashBoard.git/precache-manifest.208da89d6d863519c7f0a7f243a52466.js"
+  "/DashBoard/precache-manifest.a3465954739b719c5560a4c3d178a904.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DashBoard.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DashBoard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
