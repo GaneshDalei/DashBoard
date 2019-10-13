b554cbae3fe90533a10b24c2b2260cbd207ac0b6
ganesh <ganeshdalei1992@gmail.com>
2019-10-13 19:59:15 +0530

    Updates

--- precache-manifest.ea28b845176377568119a63d7c2dbdbf.js
+++ precache-manifest.ea28b845176377568119a63d7c2dbdbf.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6fc993e504b7ff883ac8382d88480902",
+    "revision": "018a7a47aa6c8d75d03db2d30abc2686",
     "url": "/DashBoard/index.html"
   },
   {
-    "revision": "a8dc425378e85b674560",
-    "url": "/DashBoard/static/css/main.110cf0e7.chunk.css"
+    "revision": "291c135fbf9bea1e286d",
+    "url": "/DashBoard/static/css/main.c49cbe20.chunk.css"
   },
   {
-    "revision": "4ee9e4788df417e7d3e0",
-    "url": "/DashBoard/static/js/2.c3134c68.chunk.js"
+    "revision": "48a49b84b6b834410138",
+    "url": "/DashBoard/static/js/2.d7e99b21.chunk.js"
   },
   {
-    "revision": "a8dc425378e85b674560",
-    "url": "/DashBoard/static/js/main.8318972b.chunk.js"
+    "revision": "291c135fbf9bea1e286d",
+    "url": "/DashBoard/static/js/main.a49ef13a.chunk.js"
   },
   {
     "revision": "3d32ffab5eca0665c48d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DashBoard/precache-manifest.b66dc4e0ba3c17d1ef4fdebf2d81013a.js"
+  "/DashBoard/precache-manifest.ea28b845176377568119a63d7c2dbdbf.js"
 );
 
 self.addEventListener('message', (event) => {
